8033a24b01af369ae11f5548b0cf32907e1554f4
Gustavo Siqueira <g.csiqueira@outlook.com>
2017-10-18 20:28:48 -0200

    scripts

---
+++ assets/js/lazyload.js
@@ -0,0 +1,28 @@
+/*LAZY LOAD*/
+
+var loadImages;
+
+$(document).ready(function(){
+    $(".lazy-img img").on("load", function(){
+        $(this).addClass("loaded");
+    });
+
+    $(document).on("scroll",function(){
+        loadImages();
+    });
+
+    (loadImages = function loadImages(){
+        $.each($(".lazy-img"), function(){
+            var block = $(this);
+            var image = block.find("img");
+
+            if(isOnScreen(block)){
+                var url = image.data("url");
+
+                if(image.attr("src") != url){
+                    image.attr("src",url);
+                };
+            }
+        });
+    })();
+});
